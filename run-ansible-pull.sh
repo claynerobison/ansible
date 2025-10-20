01a8bb3d4b281d88ddee732afa8fc251adacd924
Robison, Clayne B <clayne.b.robison@intel.com>
2025-10-20 12:38:58 -0700

    simplify inventory update MOTD make log easier to read.

--- run-ansible-pull.sh
+++ run-ansible-pull.sh
@@ -1,28 +1,39 @@
 #!/bin/bash
 
-# Demo script for testing ansible-pull
-# This script demonstrates how to use ansible-pull with this repository
-
+# Ansible Pull Demo Runner
 set -e
 
 REPO_URL="https://github.com/claynerobison/ansible.git"
 PLAYBOOK="demo/local.yml"
-if [ -w /var/log ]; then
-    LOGFILE="/var/log/ansible-pull.log"
-else
-    LOGFILE="$HOME/ansible-pull.log"
-    echo "Warning: /var/log is not writable. Logging to $LOGFILE"
+LOGFILE="/var/log/ansible-pull.log"
+LOCKFILE="/var/run/ansible-pull.lock"
+
+# Prevent multiple instances from running
+if [ -f "$LOCKFILE" ]; then
+    echo "$(date): Ansible-pull already running, skipping..." >> "$LOGFILE"
+    exit 0
 fi
 
-echo "Starting ansible-pull at $(date)"
+# Create lock file
+touch "$LOCKFILE"
 
-# Run ansible-pull (will only run if hostname is in inventory)
+# Cleanup function
+cleanup() {
+    rm -f "$LOCKFILE"
+}
+trap cleanup EXIT
+
+echo "$(date):************************ Starting ansible-pull" >> "$LOGFILE"
+
+# Run ansible-pull
 ansible-pull \
     --url "$REPO_URL" \
     --checkout main \
     --directory /tmp/ansible-pull \
     --inventory demo/inventory \
     --verbose \
-    "$PLAYBOOK" 2>&1 | tee -a "$LOGFILE"
+    "$PLAYBOOK" >> "$LOGFILE" 2>&1 || true
 
-echo "ansible-pull completed at $(date)"
+echo "ansible-pull completed at $(date)**********************************"
+echo " "
+echo " "
